4f97d847779d66d93bb29991f6841b0ea6ff4caf
suganuma-takuo <suganuma0606@gmail.com>
2023-04-24 17:43:19 +0900

    add 投稿削除

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,11 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     public function getPaginateByLimit(int $limit_count = 3)
     {
         return $this->orderBy('updated_at','DESC')->paginate($limit_count);
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_','-',app()->getLocale())}}">
     <head>
         <meta charset=utf-8>
-        <<title>Blog</title>
+        <title>Blog</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
@@ -15,11 +15,26 @@
                     <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                 </h2>
                 <p class='body'>{{ $post->body }}</p>
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
             </div>
             @endforeach
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <script>
+            function deletePost(id)
+            {
+                'use strict'
+                
+                if(confirm('削除すると復元できません。\n本当に削除しますか？'))
+                {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
