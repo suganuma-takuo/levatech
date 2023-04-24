3791f1665397ebf7dd134716b16fe01872bc83bd
suganuma-takuo <suganuma0606@gmail.com>
2023-04-24 14:18:53 +0900

    投稿の作成

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']); 
 
 Route::get('/posts',[PostController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::post('/posts',[PostController::class, 'store']);
+
+Route::get('/posts/{post}',[PostController::class ,'show']);
